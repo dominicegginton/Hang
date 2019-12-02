0c9624594e7bad30ddb07f93259baca84f776b3f
Dominic <eggintod@coventry.ac.uk>
2019-12-02 21:48:48 +0000

    added the ability to get delete and edit sessions

--- Hang/Models/Sessions.swift
+++ Hang/Models/Sessions.swift
@@ -25,4 +25,44 @@ class Sessions {
     public var count: Int {
         return self.sessions.count
     }
+    
+    public func add(session: Session) {
+        self.sessions.append(session)
+    }
+    
+    public func getSession(atIndex index: Int) throws -> Session {
+        if self.sessions.indices.contains(index) {
+            return self.sessions[index]
+        } else {
+            throw SessionsError.outOfRange(index)
+        }
+    }
+    
+    public func insert(session newSession: Session, atIndex index: Int) throws {
+        if self.sessions.indices.contains(index) || self.count == index {
+            self.sessions.insert(newSession, at: index)
+        } else {
+            throw SessionsError.outOfRange(index)
+        }
+    }
+    
+    public func update(session updatedSession: Session, atIndex index: Int) throws {
+        if self.sessions.indices.contains(index) {
+            self.sessions[index] = updatedSession
+        } else {
+            throw SessionsError.outOfRange(index)
+        }
+    }
+    
+    public func remove(atIndex index: Int) throws {
+        if self.sessions.indices.contains(index) {
+            self.sessions.remove(at: index)
+        } else {
+            throw SessionsError.outOfRange(index)
+        }
+    }
+    
+    enum SessionsError: Error {
+        case outOfRange(_ index: Int)
+    }
 }

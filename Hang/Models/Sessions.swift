414ec72ecb2aadba5e6d9eac96cea7e9d2ebbe67
Dominic <eggintod@coventry.ac.uk>
2019-12-06 16:14:37 +0000

    added comments to Sessions class

--- Hang/Models/Sessions.swift
+++ Hang/Models/Sessions.swift
@@ -56,40 +56,78 @@ class Session: Time, Codable {
 
 class Sessions {
     
+    // Sessions
     var sessions: [Session]
+    // Singleton instacle here.
     public static let instance = Sessions()
     
+    /**
+    Create new instance of Sessions
+
+    - Returns: A new insatnce of Sessions
+    */
     init() {
+        // Setup Instance
         self.sessions = []
         self.load()
     }
     
+    /**
+    Saves current array of sessions in user defaults
+    */
     func save() {
+        // Get encoded data
         if let data = try? JSONEncoder().encode(self.sessions) {
+            // Get JSON string frommat.
             let jsonString = String(data: data, encoding: .utf8)
-            print(">>> saving \(jsonString!)")
+            // Save JSON into user defaults
             UserDefaults.standard.set(jsonString!, forKey: "sessions")
         }
     }
     
+    /**
+        Loads current array of Sessions into user defaults
+     */
     func load() {
+        // if jason string can be read from user.defaults then decode data
         if let jsonString = UserDefaults.standard.value(forKey: "sessions") as? String {
             let decorder = JSONDecoder()
             let sessions = try? decorder.decode([Session].self, from: jsonString.data(using: .utf8)!)
+            // Set sessions
             self.sessions = sessions!
         }
     }
     
+    /**
+    Get number of sessions
+
+    - Returns: returns number repersenting the legth of the sessions array
+    */
     public var count: Int {
         return self.sessions.count
     }
     
+    /**
+    Add sessions to system
+
+    - Parameter session: The new session being addded
+
+    - Returns: A new insatnce of Session
+    */
     public func add(session: Session) throws {
         self.sessions.append(session)
         self.save()
     }
     
+    /**
+        Get session with all details.
+     
+     - Parameter atIndex: InT
+     
+     - Returns Session
+    */
     public func getSession(atIndex index: Int) throws -> Session {
+        // Check to see if index is out of rnage
         if self.sessions.indices.contains(index) {
             return self.sessions[index]
         } else {
@@ -97,7 +135,13 @@ class Sessions {
         }
     }
     
+    /**
+        Insert given session into model.
+     
+     - Parameter atIndex: Int
+    */
     public func insert(session newSession: Session, atIndex index: Int) throws {
+        // Check to see if index is out of rnage
         if self.sessions.indices.contains(index) || self.count == index {
             self.sessions.insert(newSession, at: index)
         } else {
@@ -106,17 +150,33 @@ class Sessions {
         self.save()
     }
     
+    /**
+        Update a session
+     
+     - Parameter atIndex: Int index of the session to be updated
+     - Parameter session: Session to be updated
+    */
     public func update(session updatedSession: Session, atIndex index: Int) throws {
+        // Check to see if index is out of rnage
         if self.sessions.indices.contains(index) {
+            // update session
             self.sessions[index] = updatedSession
         } else {
             throw SessionsError.outOfRange(index)
         }
+        // Save
         self.save()
     }
     
+    /**
+        Remove a session from
+     
+     - Parameter atIndex: Int repersenting the index of session to delete.
+    */
     public func remove(atIndex index: Int) throws {
+        // Check to see if index is out of rnage
         if self.sessions.indices.contains(index) {
+            // Remove
             self.sessions.remove(at: index)
         } else {
             throw SessionsError.outOfRange(index)
@@ -124,11 +184,17 @@ class Sessions {
         self.save()
     }
     
+    /**
+        Clears Sessions Array and Saves to disk
+    */
     public func clearSessions() {
+        // Remove all sessions
         self.sessions.removeAll()
+        // Save to disk
         self.save()
     }
     
+    // Error Enum
     enum SessionsError: Error {
         case outOfRange(_ index: Int)
     }

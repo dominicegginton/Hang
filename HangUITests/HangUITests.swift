61a922e20e57953a20bbe276c8894781adc50863
Dominic <eggintod@coventry.ac.uk>
2019-12-06 06:03:33 +0000

    created new ui tests to delete and edit a session, and start the timer

--- HangUITests/HangUITests.swift
+++ HangUITests/HangUITests.swift
@@ -82,5 +82,67 @@ class HangUITests: XCTestCase {
         XCTAssertTrue(!app.tables.element.cells.element(boundBy: 0).exists)
     }
     
+    func testDeleteSession() {
+        let app = XCUIApplication()
+        app.buttons["newSessionBtn"].tap()
+        XCTAssertTrue(app.navigationBars["Session"].exists)
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssertTrue(app.navigationBars["Hang"].exists)
+        XCTAssertTrue(app.tables.element.cells.element(boundBy: 0).exists)
+        app.tables.element.cells.element(boundBy: 0).swipeLeft()
+        app.tables.element.cells.element(boundBy: 0).buttons["Delete"].tap()
+        XCTAssertTrue(!app.tables.element.cells.element(boundBy: 0).exists)
+    }
     
+    func testEditSession() {
+        let app = XCUIApplication()
+        app.buttons["newSessionBtn"].tap()
+        XCTAssertTrue(app.navigationBars["Session"].exists)
+        app.textFields["sessionNameTxtBox"].tap()
+        app.textFields["sessionNameTxtBox"].typeText("Test Session")
+        app.buttons["doneBtn"].tap()
+        app.buttons["addIntervalBtn"].tap()
+        XCTAssertTrue(app.tables.element.exists)
+        app.tables.element.cells.element(boundBy: 1).tap()
+        XCTAssertTrue(app.steppers.element.exists)
+        for _ in 0...3 {
+            app.steppers.element.tap()
+        }
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssertTrue(app.tables.element.cells.element(boundBy: 0).exists)
+        XCTAssertTrue(app.navigationBars["Test Session"].exists)
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssertTrue(app.navigationBars["Hang"].exists)
+        XCTAssertTrue(app.tables.element.cells.element(boundBy: 0).exists)
+        app.tables.element.cells.element(boundBy: 0).swipeRight()
+        app.tables.element.cells.element(boundBy: 0).buttons["Edit"].tap()
+        XCTAssertTrue(app.navigationBars["Session"].exists)
+    }
+    
+    func testStartSesstionTimer() {
+        let app = XCUIApplication()
+        app.buttons["newSessionBtn"].tap()
+        XCTAssertTrue(app.navigationBars["Session"].exists)
+        app.textFields["sessionNameTxtBox"].tap()
+        app.textFields["sessionNameTxtBox"].typeText("Test Session")
+        app.buttons["doneBtn"].tap()
+        app.buttons["addIntervalBtn"].tap()
+        XCTAssertTrue(app.tables.element.exists)
+        app.tables.element.cells.element(boundBy: 1).tap()
+        XCTAssertTrue(app.steppers.element.exists)
+        for _ in 0...3 {
+            app.steppers.element.tap()
+        }
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssertTrue(app.tables.element.cells.element(boundBy: 0).exists)
+        XCTAssertTrue(app.navigationBars["Test Session"].exists)
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssertTrue(app.navigationBars["Hang"].exists)
+        XCTAssertTrue(app.tables.element.cells.element(boundBy: 0).exists)
+        app.tables.element.cells.element(boundBy: 0).tap()
+        XCTAssertTrue(app.navigationBars["00:00"].exists)
+        app.buttons["timerControlBtn"].tap()
+        sleep(10)
+        XCTAssertTrue(!app.tables.element.cells.element(boundBy: 0).exists)
+    }
 }

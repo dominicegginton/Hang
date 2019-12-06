dcfd3096a012dc696ad50bb644af5680ce2f764a
Dominic <eggintod@coventry.ac.uk>
2019-12-06 06:01:13 +0000

    created ui test for empty session

--- HangUITests/HangUITests.swift
+++ HangUITests/HangUITests.swift
@@ -1,43 +1,27 @@
-////
-////  HangUITests.swift
-////  HangUITests
-////
-////  Created by Dominic Egginton on 18/11/2019.
-////  Copyright © 2019 Dominic Egginton. All rights reserved.
-////
 //
-//import XCTest
+//  HangUITests.swift
+//  HangUITests
 //
-//class HangUITests: XCTestCase {
+//  Created by Dominic Egginton on 18/11/2019.
+//  Copyright © 2019 Dominic Egginton. All rights reserved.
 //
-//    override func setUp() {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//
-//        // In UI tests it is usually best to stop immediately when a failure occurs.
-//        continueAfterFailure = false
-//
-//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-//    }
-//
-//    override func tearDown() {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    func testExample() {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use recording to get started writing UI tests.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testLaunchPerformance() {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
-//}
+
+import XCTest
+
+class HangUITests: XCTestCase {
+
+    override func setUp() {
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    func testAddEmptySession() {
+        let app = XCUIApplication()
+        app.buttons["newSessionBtn"].tap()
+        XCTAssertTrue(app.navigationBars["Session"].exists)
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssertTrue(app.navigationBars["Hang"].exists)
+        XCTAssertTrue(app.tables.element.cells.element(boundBy: 0).exists)
+    }
+    
+}
